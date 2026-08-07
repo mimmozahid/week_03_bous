5e9ecf3f8f0882b61408bda6f0d4fe04daba23ae
mimmozahid <mdmimmozahid@gmail.com>
2026-08-07 23:42:52 +0600

    Add implementation for Local: PBDS problem and update C. Two TVs problem format

---
+++ PBDS.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace __gnu_pbds;
+using namespace std;
+using ll = long long;
+
+template <typename T> using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>; 
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n;
+    cin >> n;
+
+    pbds <int> p;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        p.insert (x);
+    }
+    
+    for (auto x : p)
+        cout << x << " ";
+    cout << endl;
+    
+    return 0;
+}
